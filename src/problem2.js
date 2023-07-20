55aed4262576e5f8c86821c61cd4975b82018cd8
KimBitnari <nari6726@gmail.com>
2023-07-21 08:47:15 +0900

    [JavaScript] problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,26 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let idx1 = 0, idx2 = 1;
+  let str = [cryptogram[0]];
+
+  while (idx2 < cryptogram.length) {
+    if (str[idx1] === cryptogram[idx2]) {
+      str.pop();
+      idx1--;
+    }
+    else {
+      str.push(cryptogram[idx2]);
+      idx1++;
+    }
+
+    idx2++;
+  }
+
+  str = str.join('');
+
+  return str;
 }
+
+// console.log(problem2("browoanoommnaon"));
+// console.log(problem2("zyelleyz"));
 
 module.exports = problem2;
