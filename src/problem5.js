1d6538bf761cca09fcae5d9e08c85360af167c38
KimBitnari <nari6726@gmail.com>
2023-07-25 23:43:02 +0900

    [JavaScript] problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,18 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let result = [];
+  let m = money;
+
+  for (let i = 0; i < moneyType.length; i++) {
+    const div = parseInt(m / moneyType[i]);
+    m %= moneyType[i];
+    result.push(div);
+  }
+
+  return result;
 }
+
+// console.log(problem5(50237));
+// console.log(problem5(15000));
 
 module.exports = problem5;
